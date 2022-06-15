0ab72d048f9582b4cad79197ed71346c0edd08c1
Moein Mirkiani <moein.mirkiani@gmail.com>
2022-06-15 23:37:08 +0430

    updated router settings to show proper page title for each page

--- src/router/index.js
+++ src/router/index.js
@@ -1,17 +1,23 @@
-import {createRouter, createWebHistory} from 'vue-router'
+import { createRouter, createWebHistory } from 'vue-router'
 import HomeView from '@/views/HomeView.vue'
 
 const routes = [
     {
         path: '/',
         name: 'home',
-        component: HomeView
+        component: HomeView,
+        meta: { title: 'KRYPTON' }
     }
 ]
 
 const router = createRouter({
     history: createWebHistory(),
     routes
+})
+
+router.beforeEach((to, next) => {
+    document.title = to.meta.title
+    next
 })
 
 export default router
